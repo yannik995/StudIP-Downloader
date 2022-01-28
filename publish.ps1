08294d3cd93887fb1c2e9872050a08beda819d93
Yannik Eichel <git@yannikeichel.de>
2022-01-28 17:48:08 +0100

    Build Script

---
+++ publish.ps1
@@ -0,0 +1,7 @@
+$array = @("linux-x64","win-x64","osx-x64")
+
+for ($i=0; $i -lt $array.length; $i++){
+    $enviroment = $array[$i]   
+	dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained false
+	Compress-Archive -Path ($PSScriptRoot + "\StudIPDownloader\bin\Debug\netcoreapp3.1\" + $enviroment + "\publish\*") -Force -DestinationPath ("StudIP-Downloader_" + $enviroment + ".zip")
+}
