15d293a5edced1eef86fdeb8b825a39a7e57deb3
Yannik Eichel <git@yannikeichel.de>
2022-01-28 18:05:32 +0100

    Fix Build

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
-$array = @("linux-x64","win-x64","osx-x64")
+$array = @("win-x64","linux-x64","osx-x64")
 
 for ($i=0; $i -lt $array.length; $i++){
     $enviroment = $array[$i]   
-	dotnet publish -r $enviroment -p:PublishSingleFile=true --self-contained false
+	dotnet publish -r $enviroment -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
 	Compress-Archive -Path ($PSScriptRoot + "\StudIPDownloader\bin\Debug\netcoreapp3.1\" + $enviroment + "\publish\*") -Force -DestinationPath ("StudIP-Downloader_" + $enviroment + ".zip")
 }
